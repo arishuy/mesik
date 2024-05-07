019f13b480d7d5e5dadb829d448c0fcf1e3bdd6e
arishuy <huybui19898@gmail.com>
2024-05-07 09:53:45 +0700

    Refactor DetailArtist component to improve UI and functionality

--- src/pages/DetailArtist/DetailArtist.jsx
+++ src/pages/DetailArtist/DetailArtist.jsx
@@ -1,6 +1,6 @@
-import { Avatar, Card, Grid, Stack, Typography } from '@mui/material'
+import { Avatar, Card, Grid, IconButton, Stack, Typography } from '@mui/material'
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { useMusicPlayer } from '../../contexts/music.context'
 import moment from 'moment'
 import AxiosInterceptors from '../../common/utils/axiosInterceptors'
@@ -9,10 +9,13 @@ import Empty from '../../common/components/Empty'
 import Loading from '../../common/components/Loading/Loading'
 import { Helmet } from 'react-helmet-async'
 import SongCardVer2 from '../../common/components/SongCard_Ver2'
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined'
+import PlayCircleFilledWhiteOutlinedIcon from '@mui/icons-material/PlayCircleFilledWhiteOutlined'
 
 const DetailArtist = () => {
   const id = useParams()
   const { playSong } = useMusicPlayer()
+  const navigation = useNavigate()
   const [artist, setArtist] = useState({})
   const [isLoading, setIsLoading] = useState(true)
   const [allPlaylists, setAllPlaylists] = React.useState([])
@@ -63,9 +66,7 @@ const DetailArtist = () => {
       >
         <Stack direction='row' spacing={5} justifyContent='space-around' alignItems='center'>
           <Stack direction='column' spacing={3}>
-            <Typography variant='h1'>
-              {artist.user?.first_name} {artist.user?.last_name}
-            </Typography>
+            <Typography variant='h1'>{artist.display_name}</Typography>
             <Typography variant='h4'>{artist.descriptions}</Typography>
           </Stack>
           <Avatar alt='Remy Sharp' src={artist.user?.photo_url} sx={{ width: 300, height: 300 }} />
@@ -88,30 +89,30 @@ const DetailArtist = () => {
       {artist.albums?.length === 0 ? (
         <Empty message={'Không có album nào'} />
       ) : (
-        <Stack direction='row' spacing={2}>
+        <Grid container spacing={3}>
           {artist.albums?.map((album) => (
-            <Card
-              sx={{
-                padding: '20px',
-                '&:hover': {
-                  backgroundColor: '#f0f0f0',
-                  cursor: 'pointer',
-                  transform: 'scale(1.05)'
-                }
-              }}
-              key={album._id}
-              onClick={() => {
-                playSong(album.songs)
-              }}
-            >
-              <img src={album.photo_url} alt='album' width='250px' />
-              <Typography variant='h6' pt={2}>
-                {album.title}
-              </Typography>
-              <Typography variant='body2'>{moment(album.updatedAt).format('YYYY')}</Typography>
-            </Card>
+            <Grid item xs={12} md={6} lg={3} key={album.id}>
+              <div className='song-card'>
+                <img className='song-card_image' src={album.photo_url} alt='David Bowie - Aladdin Sane' />
+                <div className='song-card_info'>
+                  <div className='song-card_info_artist'>{album.songs.length} bài hát</div>
+                  <div className='song-card_info_album'> {moment(album.createdAt).format('DD/MM/YYYY')}</div>
+                  <div className='song-card_info_title'>{album.title}</div>
+                </div>
+                <div className='song-card_play'>
+                  <Stack direction='row' spacing={1} pt={2}>
+                    <IconButton onClick={() => playSong(album.songs)} color='success'>
+                      <PlayCircleFilledWhiteOutlinedIcon />
+                    </IconButton>
+                    <IconButton onClick={() => navigation(`/album/${album._id}`)} color='primary'>
+                      <InfoOutlinedIcon />
+                    </IconButton>
+                  </Stack>
+                </div>
+              </div>
+            </Grid>
           ))}
-        </Stack>
+        </Grid>
       )}
     </div>
   )
